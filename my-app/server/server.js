5961a6227b02a7fdc1f46829ae6373a6e44c9e27
czaczka <89947179+czaczka@users.noreply.github.com>
2022-09-05 16:45:27 +1000

    created server.js

---
+++ my-app/server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+app = express();
+
+fs = require('fs'),
+    http = require('http'),
+    PORT = 3000;
+
+const cors = require('cors');
+
+app.use(cors());
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({
+    extended: true}));
+
+app.use(bodyParser.json());
+
+app.use(express.static(__dirname + '/../dist/my-app'));
+
+const httpServer = http.Server(app);
+
+httpServer.listen(PORT, function() {
+    console.log(`http Server listening on port: ${PORT}`);
+});
