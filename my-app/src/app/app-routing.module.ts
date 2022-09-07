d15d281536a6d005521f57f3b505eac1606a3dba
czaczka <89947179+czaczka@users.noreply.github.com>
2022-09-07 10:59:05 +1000

    added profile and chat component and routing for modules

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ChatComponent } from './chat/chat.component';
 
-const routes: Routes = [{path: 'login', component:LoginComponent}];
+const routes: Routes = [{path: 'login', component:LoginComponent}, {path: 'profile', component:ProfileComponent}, {path: 'chat', component:ChatComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
